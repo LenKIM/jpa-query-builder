07b643981b4706b7063d2bf185005726ec0117b5
LenKIM <joenggyu0@gmail.com>
2026-04-06 00:37:29 +0900

    feat: implement query execution with parameter binding and add tests

--- src/main/java/persistence/QueryExecutor.java
+++ src/main/java/persistence/QueryExecutor.java
@@ -1,6 +1,8 @@
 package persistence;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -17,29 +19,48 @@ public class QueryExecutor {
      * SELECT 쿼리 실행 및 자동 매핑
      */
     public <T> List<T> query(String sql, Class<T> resultClass, Object... params)
-            throws SQLException {
-        return null;
-    };
+            throws Exception {
+        PreparedStatement pstm = connection.prepareStatement(sql);
+
+        for (int i = 0; i < params.length; i++) {
+            pstm.setObject(i + 1, params[i]);
+        }
+
+        ResultSet resultSet = pstm.executeQuery();
+        return mapper.mapToList(resultSet, resultClass);
+    }
+
+    ;
 
     /**
      * Named Parameter 쿼리 실행
      */
     public <T> List<T> query(NamedParameterQuery query, Class<T> resultClass)
-            throws SQLException {
-        return null;
-    };
+            throws Exception {
+        PreparedStatement pstm = connection.prepareStatement(query.toJdbcSql());
+
+        query.bindParameters(pstm);
+        ResultSet resultSet = pstm.executeQuery();
+        return mapper.mapToList(resultSet, resultClass);
+    }
 
     /**
      * INSERT/UPDATE/DELETE 실행
      */
     public int execute(String sql, Object... params) throws SQLException {
-        return 1;
-    };
+        PreparedStatement pstm = connection.prepareStatement(sql);
+        for (int i = 0; i < params.length; i++) {
+            pstm.setObject(i + 1, params[i]);
+        }
+        return pstm.executeUpdate();
+    }
 
     /**
      * Named Parameter로 실행
      */
     public int execute(NamedParameterQuery query) throws SQLException {
-        return 1;
-    };
+        PreparedStatement pstm = connection.prepareStatement(query.toJdbcSql());
+        query.bindParameters(pstm);
+        return pstm.executeUpdate();
+    }
 }
